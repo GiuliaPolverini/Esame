19334291ac66b838d711a2309826e1a5e8acb96b
GiuliaPolverini <giulia.polverini@studio.unibo.it>
2020-10-08 17:02:03 +0200

    nothing

--- Version_08-10.py
+++ Version_08-10.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Oct  8 12:28:56 2020
+Created on Thu Oct  8 16:45:36 2020
 
 @author: Giulia
 """
@@ -62,16 +62,17 @@ class Pendulum:
         new_position = self.polar_to_cartesian()
         self.trajectory.append(new_position)
         return new_position
+    
 class Animator:
-    def __init__(self, pendulum, draw_trace=False):
+    def __init__(self, pendulum, draw_trace = False):
         self.pendulum = pendulum
         self.draw_trace = draw_trace
         self.time = 0.0
  
         # set up the figure
         self.fig, self.ax = plt.subplots()
-        self.ax.set_ylim(-3, 3)
-        self.ax.set_xlim(-3, 3)
+        self.ax.set_ylim(-2.5, 2.5)
+        self.ax.set_xlim(-2.5, 2.5)
  
         # prepare a text window for the timer
         self.time_text = self.ax.text(0.05, 0.95, '', 
@@ -87,8 +88,6 @@ class Animator:
         # trace the whole trajectory of the second pendulum mass
         if self.draw_trace:
             self.trace, = self.ax.plot(
-                            [a[1, 0] for a in self.pendulum.trajectory],
-              	            [a[1, 1] for a in self.pendulum.trajectory],
                             [a[2, 0] for a in self.pendulum.trajectory],
               	            [a[2, 1] for a in self.pendulum.trajectory])
 
@@ -104,8 +103,6 @@ class Animator:
         self.line.set_xdata(data[:, 0])
          
         if self.draw_trace:
-            self.trace.set_xdata([a[1, 0] for a in self.pendulum.trajectory])
-            self.trace.set_ydata([a[1, 1] for a in self.pendulum.trajectory])
             self.trace.set_xdata([a[2, 0] for a in self.pendulum.trajectory])
             self.trace.set_ydata([a[2, 1] for a in self.pendulum.trajectory])
         return self.line,
@@ -113,39 +110,8 @@ class Animator:
     def animate(self):
         self.animation = animation.FuncAnimation(self.fig, self.update,
                          self.advance_time_step, interval=25, blit=False)
-        
+
 pendulum = Pendulum(theta1=np.pi, theta2=np.pi-0.05, dt=0.02)
 animator = Animator(pendulum=pendulum, draw_trace=True)
 animator.animate()
 plt.show()
-
-
-#%%
-
-#Codice per plottare gli andamenti delle 2 masse (insieme)
-trajectory = np.asarray(pendulum.trajectory)
-fig = plt.figure()
-
-for mass in [1, 2]:
-    plt.plot(trajectory[:,mass,0], 
-             trajectory[:,mass,1])
-
-#%%
-    
-delta_th2 = np.linspace(0, 0.99, 100)
-traj_list = []
-mass = 2
-
-for d in delta_th2:
-    pend = Pendulum(theta1 = np.pi, theta2 = np.pi/2 - d, dt = 0.01)
-    
-    #per ognuno dei 100 pendoli "creati sopra" si fanno 1000 evoluzioni
-    for i in range(0, n_evolution):
-        pend.evolve()
-    
-    traj = np.asarray(pend.trajectory) #conversione
-    traj_mass2 = traj[:, mass, :] #slicing 2°massa
-    traj_list.append(traj_mass2) #salvataggio
-
-for traj in traj_list:
-    plt.plot(traj[:,0],traj[:,1])
