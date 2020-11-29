c37ff8556a9873a55af24a6d2652cba3158e857b
GiuliaPolverini <giulia.polverini@studio.unibo.it>
2020-11-29 16:29:47 +0100

    Trace of phase space

--- Animator.py
+++ Animator.py
@@ -40,40 +40,49 @@ class Animator:
             self.x2 = pend_simulation.x2
             self.y2 = pend_simulation.y2
 
-            # trace in 2nd mass' phase space
+            # tracing the second mass' phase space for θ and p
+            self.theta1 = pend_simulation.integrate[:,1]
+            self.p1 = pend_simulation.integrate[:,3]
             self.theta2 = pend_simulation.integrate[:,1]
-            self.p2     = pend_simulation.integrate[:,3]
+            self.p2 = pend_simulation.integrate[:,3]
 
         if (n_frames == -1) or (n_frames > self.x1.shape[0]):
             self.n_frames = self.x1.shape[0]
         else: self.n_frames = n_frames
 
-            # OLD DINAMICAL VERSION WITH PROBLEMS WHEN MIN=MAX
-            # boundary: fits the size of the animation window to the length of
-            # the pendulum plus its 20%
-            # self.bound = 0.2
-            # x2_extr = np.asarray([np.min(self.x2), np.max(self.x2)]) * (1 + self.bound)
-            # y2_extr = np.asarray([np.min(self.y2), np.max(self.y2)]) * (1 + self.bound)
-            #
-            # # set up the figure
-            # self.fig, self.ax = plt.subplots(figsize = (8,8))
-            # self.ax.set_xlim(*x2_extr) # (left = x2_extr[0], right = x2_extr[1])
-            # self.ax.set_ylim(*y2_extr)
-
-        # STATIC VERSION
         l = pend_simulation.l
-        #self.fig, self.ax = plt.subplots(figsize = (8,8))
+
         self.fig, (self.ax1, self.ax2) = plt.subplots(1,2, figsize = (8,16))
 
         #PLOT for cartesin trajectory
-        self.ax1.set_title('blabla', fontsize=18)
-        self.ax1.set_xlabel('blabla_x', fontsize=18)
-        self.ax1.set_xlim(left= -2.1*l,  right=2.1*l)
+        self.ax1.set_title('Cartesian Space', fontsize=10)
+        #self.ax1.set_xlabel('x', fontsize=5)
+        #self.ax1.set_ylabel('y', fontsize=5)
+        self.ax1.set_xlim(left=-2.1*l, right=2.1*l)
         self.ax1.set_ylim(bottom=-2.1*l, top=2.1*l)
 
-        #PLOT phase space traj 2nd mass
-        self.ax2.set_title('blabla2', fontsize=18)
+        #PLOT for phase space trajectory
+        self.ax2.set_title('Phase Space', fontsize=10)
 
+        theta1_min = np.min(self.theta1)
+        if theta1_min > -np.pi: theta1_min = -np.pi
+        else: theta1_min = np.floor(theta1_min/np.pi)*np.pi
+
+        theta1_max = np.max(self.theta1)
+        if theta1_max < np.pi: theta1_max = np.pi
+        else: theta1_max = np.ceil(theta1_max/np.pi)*np.pi
+
+        p1_min = np.min(self.p1)
+        p1_max = np.max(self.p1)
+        
+        if np.isclose(p1_min, p1_max, rtol=1e-05, atol=1e-08):
+            p1_min = p1_min - np.abs(p1_min)/2
+            p1_max = p1_max + np.abs(p1_max)/2
+        self.ax2.set_ylim(bottom=p1_min, top=p1_max)
+        
+        self.ax2.set_xlim(left=theta1_min,  right=theta1_max) # theta2
+        self.ax2.set_ylim(bottom=p1_min*1.1, top=p1_max*1.1)
+        
         theta2_min = np.min(self.theta2)
         if theta2_min > -np.pi: theta2_min = -np.pi
         else: theta2_min = np.floor(theta2_min/np.pi)*np.pi
@@ -90,9 +99,9 @@ class Animator:
             p2_max = p2_max + np.abs(p2_max)/2
         self.ax2.set_ylim(bottom=p2_min, top=p2_max)
 
-        self.ax2.set_xlim(left=theta2_min,  right=theta2_max) #<-- theta 2
+        self.ax2.set_xlim(left=theta2_min,  right=theta2_max) # theta2
         self.ax2.set_ylim(bottom=p2_min*1.1, top=p2_max*1.1)
-        #self.ax2.set_ylim(bottom=-20, top=20) #<-- p 2
+        #self.ax2.set_ylim(bottom=-20, top=20) # p2
 
         # prepare a text window for the timer
         self.time_text = self.ax1.text(0.05, 0.95,'Elapsed time: {:6.2f} s'.format(0.0),
@@ -106,19 +115,22 @@ class Animator:
                                    [0.0, self.y1[0], self.y2[0]],
                                    marker = 'o')
 
-        self.dot, = self.ax2.plot( self.theta2[0], self.p2[0],
+        self.dot, = self.ax2.plot([self.theta1[0], self.p1[0]],
+                                  [self.theta2[0], self.p2[0]],
                                    marker = 'o')
 
         # trace the whole trajectory of both the masses
         if self.draw_trace:
             self.trace1, = self.ax1.plot(self.x1[0], self.y1[0])
             self.trace2, = self.ax1.plot(self.x2[0], self.y2[0])
-            self.phase_trace, = self.ax2.plot(self.theta2[0], self.p2[0])
+            self.phase_trace1, = self.ax2.plot(self.theta1[0], self.p1[0])
+            self.phase_trace2, = self.ax2.plot(self.theta2[0], self.p2[0])
 
         else:
             self.trace1 = None
             self.trace2 = None
-            self.phase_trace = None
+            self.phase_trace1 = None
+            self.phase_trace2 = None
 
 
     def update(self, n):
@@ -141,8 +153,8 @@ class Animator:
         self.line.set_xdata([0.0, self.x1[n], self.x2[n]])
         self.line.set_ydata([0.0, self.y1[n], self.y2[n]])
 
-        self.dot.set_xdata(self.theta2[n])
-        self.dot.set_ydata(self.p2[n])
+        self.dot.set_xdata([self.theta1[n], self.theta2[n]])
+        self.dot.set_ydata([self.p1[n], self.p2[n]])
 
         # update of each trace for both the pendulums
         if self.draw_trace:
@@ -152,10 +164,14 @@ class Animator:
             self.trace2.set_xdata(self.x2[0: n+1])
             self.trace2.set_ydata(self.y2[0: n+1])
 
-            self.phase_trace.set_xdata(self.theta2[0: n+1])
-            self.phase_trace.set_ydata(self.p2[0: n+1])
+            self.phase_trace1.set_xdata(self.theta1[0: n+1])
+            self.phase_trace1.set_ydata(self.p1[0: n+1])
+            
+            self.phase_trace2.set_xdata(self.theta2[0: n+1])
+            self.phase_trace2.set_ydata(self.p2[0: n+1])
 
-        return self.line, self.dot, self.trace1, self.trace2, self.phase_trace
+        return self.line, self.dot, self.trace1, self.trace2,
+        self.phase_trace1, self.phase_trace2
 
     def animate(self):
 
